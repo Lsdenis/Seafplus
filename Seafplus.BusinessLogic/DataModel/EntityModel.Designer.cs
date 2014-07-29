8067d52574b107e2476714fe4ff77939a4f9edff
Denis P <lsden1995@gmail.com>
2014-07-29 16:39:16 +0300

    Generate Entity

--- Seafplus.BusinessLogic/DataModel/EntityModel.Designer.cs
+++ Seafplus.BusinessLogic/DataModel/EntityModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'D:\Projects\seafplus\Seafplus\Seafplus\Seafplus.BusinessLogic\DataModel\EntityModel.edmx'. 
+﻿// Default code generation is disabled for model 'D:\Git\Seafplus\Seafplus.BusinessLogic\DataModel\EntityModel.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
